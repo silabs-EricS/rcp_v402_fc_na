8e19f56e419664db01f3c3883039696369319802
silabs-EricS <eric.su@silabs.com>
2022-05-11 11:28:53 +0800

    flow control work

--- gecko_sdk_4.0.2/protocol/bluetooth/bgstack/ll/utils/hci_packet/inc/sl_btctrl_hci_packet.h
+++ gecko_sdk_4.0.2/protocol/bluetooth/bgstack/ll/utils/hci_packet/inc/sl_btctrl_hci_packet.h
@@ -3,13 +3,28 @@
 
 #include "em_common.h"
 
-#define hci_command_header_size     3   // opcode (2 bytes), length (1 byte)
-#define hci_acl_data_header_size    4   // handle (2 bytes), length (2 bytes)
+#define hci_command_header_size       3   // opcode (2 bytes), length (1 byte)
+#define hci_acl_data_header_size      4   // handle (2 bytes), length (2 bytes)
+#define hci_flow_control_config_index 10
+#define hci_max_payload_size          256
+
+#define ENABLE_HCI_CONTROLLER_TO_HOST_FLOW_CONTROL
+
+/* OpCode Group Fields */
+#define BT_OGF_LINK_CTRL                        0x01
+#define BT_OGF_BASEBAND                         0x03
+#define BT_OGF_INFO                             0x04
+#define BT_OGF_STATUS                           0x05
+#define BT_OGF_LE                               0x08
+#define BT_OGF_VS                               0x3f
+/* Construct OpCode from OGF and OCF */
+#define BT_OP(ogf, ocf)                         ((ocf) | ((ogf) << 10))
 
 SL_PACK_START(1)
 typedef struct {
   uint16_t opcode; /* HCI command opcode */
   uint8_t param_len; /* command parameter length */
+  uint8_t payload[hci_max_payload_size];
 } SL_ATTRIBUTE_PACKED hci_command_t;
 SL_PACK_END()
 
@@ -17,13 +32,17 @@ SL_PACK_START(1)
 typedef struct {
   uint16_t conn_handle; /* ACL connection handle */
   uint16_t length; /* Length of packet */
+  uint8_t payload[hci_max_payload_size];
 } SL_ATTRIBUTE_PACKED acl_packet_t;
 SL_PACK_END()
 
 enum hci_packet_type {
   hci_packet_type_ignore = 0, /* 0 used to trigger wakeup interrupt */
   hci_packet_type_command = 1,
-  hci_packet_type_acl_data = 2
+  hci_packet_type_acl_data = 2,
+  hci_packet_type_syhc_data = 3,
+  hci_packet_type_event = 4,
+  hci_packet_type_iso_data = 5
 };
 
 enum hci_packet_state {
@@ -32,11 +51,58 @@ enum hci_packet_state {
   hci_packet_state_read_data = 2
 };
 
+enum hci_packet_opcode {
+  HCI_Read_Local_Supported_Commands            = BT_OP(BT_OGF_INFO, 0x0002),
+  HCI_Set_Host_Controller_To_Host_Flow_Control = BT_OP(BT_OGF_BASEBAND, 0x0031),
+  HCI_Host_Buffer_Size                         = BT_OP(BT_OGF_BASEBAND, 0x0033),
+  HCI_Host_Number_Of_Completed_Packets         = BT_OP(BT_OGF_BASEBAND, 0x0035)
+};
+
+enum hci_packet_event {
+  HCI_Disconnection_Complete                    = 0x05,
+  HCI_Command_Complete                          = 0x0E
+};
+
+SL_PACK_START(1)
+typedef struct {
+  uint8_t  enable;
+} SL_ATTRIBUTE_PACKED hci_controller_to_host_flow_control_t;
+SL_PACK_END()
+
+SL_PACK_START(1)
+typedef struct {
+  uint16_t acl_mtu;
+  uint8_t  sco_mtu;
+  uint16_t acl_pkts;
+  uint16_t sco_pkts;
+} SL_ATTRIBUTE_PACKED hci_host_buffer_t;
+SL_PACK_END()
+
+SL_PACK_START(1)
+typedef struct {
+  uint8_t  handle;
+  uint16_t connection[1];
+  uint16_t completed_packet[1];
+} SL_ATTRIBUTE_PACKED hci_host_completed_t;
+SL_PACK_END()
+
+SL_PACK_START(1)
+typedef struct {
+  uint8_t eventcode; /* HCI event code */
+  uint8_t param_len; /* event parameter length */
+  enum hci_packet_type packet_type : 8;
+  uint16_t opcode;
+  uint8_t status;
+  uint8_t payload[hci_max_payload_size];
+} SL_ATTRIBUTE_PACKED hci_evt_t;
+SL_PACK_END()
+
 SL_PACK_START(1)
 typedef struct {
   enum hci_packet_type packet_type : 8;
   union {
     hci_command_t hci_cmd;
+    hci_evt_t hci_evt;
     acl_packet_t acl_pkt;
   };
 } SL_ATTRIBUTE_PACKED hci_packet_t;
--- gecko_sdk_4.0.2/protocol/bluetooth/bgstack/ll/utils/hci_packet/uart/src/sl_hci_packet.c
+++ gecko_sdk_4.0.2/protocol/bluetooth/bgstack/ll/utils/hci_packet/uart/src/sl_hci_packet.c
@@ -20,6 +20,15 @@ static uint16_t bytes_remaining; // first message byte contains packet type
 static uint16_t total_bytes_read;
 static uint16_t buffer_remaining;
 
+#ifdef ENABLE_HCI_CONTROLLER_TO_HOST_FLOW_CONTROL
+static uint8_t acl_packet_support_counter;
+static hci_controller_to_host_flow_control_t flow_control;
+static hci_host_buffer_t host_buffer;
+static hci_host_completed_t host_completed;
+
+extern void sl_set_rx_enable(bool en);
+#endif
+
 static void reset(void)
 {
   memset(hci_rx_buffer, 0, RX_BUFFER_LEN);
@@ -137,14 +146,74 @@ void sl_btctrl_hci_packet_read(void)
     }
     case hci_packet_state_read_data:
     {
+      // TODO: if HCI flow control related command is received, process it here, and DO NOT forward it to the controller
+      //         * if HCI_Set_Host_Controller_To_Host_Flow_Control (OGF 0x03 OCF 0x0031) is received,
+      //             * enable/disable flow control based on the least significant bit (0x01) of Flow_Control_Enable
+      //             * if flow control is disabled, free the local buffer
+      //             * send HCI_Command_Complete event
+      //         * if HCI_Host_Buffer_Size message (OGF 0x03 OCF 0x0033) is received, then
+      //             * set the host_buffer_slot_size to Host_ACL_Data_Packet_Length
+      //             * set the host_total_buffer_size to Host_Total_Num_ACL_Data_Packets
+      //             * set the host_available_buffer_size to host_total_buffer_size
+      //             * init local buffer with slot size set to host_buffer_slot_size + 4 (header)
+      //             * send HCI_Command_Complete event
+      //         * if HCI_Host_Number_Of_Completed_Packets (OGF 0x03 OCF 0x0035) is received, then
+      //             * increase the host_available_buffer_size according to Host_Num_Completed_Packets
+      //                 * ideally this should be set separately for each HCI connection handle, but in first round we can use a
+      //                       common host_available_buffer_size, assuming that there is only one connection
+      //             * transmit as many packets from the controller buffer as possible
+
       if (bytes_remaining > 0) {
         hci_common_transport_receive(hci_rx_buffer, RX_BUFFER_LEN, false);
         buffer_remaining = RX_BUFFER_LEN;
         buf_byte_ptr = hci_rx_buffer;
       } else {
         sl_hci_disable_sleep(false);
+
+        #ifdef ENABLE_HCI_CONTROLLER_TO_HOST_FLOW_CONTROL
+        if (PACKET->packet_type == hci_packet_type_command &&
+          (PACKET->hci_cmd.opcode == HCI_Set_Host_Controller_To_Host_Flow_Control ||
+           PACKET->hci_cmd.opcode == HCI_Host_Buffer_Size ||
+           PACKET->hci_cmd.opcode == HCI_Host_Number_Of_Completed_Packets)) {
+          switch (PACKET->hci_cmd.opcode){
+            case HCI_Set_Host_Controller_To_Host_Flow_Control:
+            {
+              memset(&flow_control, 0, sizeof(hci_controller_to_host_flow_control_t));
+              if(PACKET->hci_cmd.param_len == sizeof(hci_controller_to_host_flow_control_t)) //Prevent the overflow
+                memcpy(&flow_control, PACKET->hci_cmd.payload, PACKET->hci_cmd.param_len);
+              if(flow_control.enable == 0){
+                acl_packet_support_counter = 0;
+              }
+              break;
+            }
+            case HCI_Host_Buffer_Size:
+            {
+              memset(&host_buffer, 0, sizeof(hci_host_buffer_t));
+              if(PACKET->hci_cmd.param_len == sizeof(hci_host_buffer_t)) //Prevent the overflow
+                memcpy(&host_buffer, PACKET->hci_cmd.payload, PACKET->hci_cmd.param_len);
+              acl_packet_support_counter = host_buffer.acl_pkts;
+              break;
+            }
+            case HCI_Host_Number_Of_Completed_Packets:
+            {
+              memset(&host_completed, 0, sizeof(hci_host_completed_t));
+              if(PACKET->hci_cmd.param_len == sizeof(hci_host_completed_t)) //Prevent the overflow
+                memcpy(&host_completed, PACKET->hci_cmd.payload, PACKET->hci_cmd.param_len);
+
+              acl_packet_support_counter += host_completed.completed_packet[0];
+              if(acl_packet_support_counter > host_buffer.acl_pkts)
+                acl_packet_support_counter = host_buffer.acl_pkts;
+              break;
+            }
+            default:
+              break;
+          }
+        }
+        #endif
+
         hci_common_transport_receive(hci_rx_buffer, RX_BUFFER_LEN - buffer_remaining, true);
         reset();
+
       }
       break;
     }
@@ -158,6 +227,57 @@ void sl_btctrl_hci_packet_read(void)
 
 uint32_t hci_common_transport_transmit(uint8_t *data, int16_t len)
 {
+  #ifdef ENABLE_HCI_CONTROLLER_TO_HOST_FLOW_CONTROL
+  //TODO: check the packet type.
+  //        * if it is an event, then we can simply transmit it.
+  //           * if a HCI_Disconnection_Complete event was sent out, then the host_available_buffer_size should be set to host_total_buffer_size (reset)
+  //        * If it is ACL data, then we should check if there is available slot on the host
+  //           * if there is available slot on the host, transmit the message and decrease the host_available_buffer_size
+  //           * if there is no available slot, put the message into the local buffer
+  //           * in both cases, check if the length (excluding the 4 byte header!) is greater than the host_buffer_slot_size, and if it is,
+  //                then fragment the data into smaller chunks, so that they fit into one slot
+
+  hci_packet_t transmit_data;
+  memcpy(&transmit_data, data, len);
+  if (transmit_data.packet_type == hci_packet_type_event) {
+    if (transmit_data.hci_evt.eventcode == HCI_Disconnection_Complete) {
+      acl_packet_support_counter = 0;
+    }
+
+    if(transmit_data.hci_evt.eventcode == HCI_Command_Complete){
+      if (transmit_data.hci_evt.opcode == HCI_Read_Local_Supported_Commands) {
+        transmit_data.hci_evt.payload[hci_flow_control_config_index] |= 0xE0;  //Set bit5,6,7
+      }else if((transmit_data.hci_evt.opcode == HCI_Set_Host_Controller_To_Host_Flow_Control)
+             ||(transmit_data.hci_evt.opcode == HCI_Host_Buffer_Size)
+             ||(transmit_data.hci_evt.opcode == HCI_Host_Number_Of_Completed_Packets)){
+        transmit_data.hci_evt.status = 0x00; //Reply the host that controller support flow control
+      }
+      memcpy(data, &transmit_data, len);
+    }
+  } else if (transmit_data.packet_type == hci_packet_type_acl_data){
+    //uint16_t ACL_packet_total_len = ((hci_packet_t *)data)->acl_pkt.length;
+    // fragment the packet into chunks and then either transmit the chunks or put them into the local buffer
+
+    if (flow_control.enable){
+        //Limit on Host_ACL_Data_Packet_Length,fragment the packet in next step
+        if(len > host_buffer.acl_mtu)
+          return -1;
+
+      //Limit on Host_Total_Num_ACL_Data_Packets
+      if(acl_packet_support_counter > 0){
+        sl_set_rx_enable(true);
+        acl_packet_support_counter--;
+      }else{
+        sl_set_rx_enable(false);
+        //Stop RX here is not good, just need to find the entry point that run out of Link Layer buffer
+        //Here need buffer for the packet, then it can re-send while host side buffer released.
+        return -1;
+      }
+
+    }
+  }
+  #endif
+
   return sl_hci_uart_write(data, len);
 }
 
